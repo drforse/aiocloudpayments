90b417829d44a706e43d9bd0bc1f384af709dd5d
drforse <george.lifeslice@gmail.com>
2021-12-21 00:06:29 +0300

    fix typo in post3ds (was posts3ds)

--- aiocloudpayments/endpoints/payments/cards/__init__.py
+++ aiocloudpayments/endpoints/payments/cards/__init__.py
@@ -1,4 +1,4 @@
 from .auth import CpCardsAuthEndpoint
 from .charge import CpCardsChargeEndpoint
-from .posts3ds import CpCardsPost3dsEndpoint
+from .post3ds import CpCardsPost3dsEndpoint
 from .topup import CpCardsTopupEndpoint
