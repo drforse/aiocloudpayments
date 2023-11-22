723647bea987fd9e88e8e5d57cbf96bb2e7f721a
abel1502 <terragame.abel@gmail.com>
2023-11-22 20:13:56 +0300

    Update token charge api

--- aiocloudpayments/__init__.py
+++ aiocloudpayments/__init__.py
@@ -1,4 +1,4 @@
 from .client import AioCpClient
 from .dispatcher import AiohttpDispatcher, Router, Result
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
--- aiocloudpayments/endpoints/payments/tokens/bases.py
+++ aiocloudpayments/endpoints/payments/tokens/bases.py
@@ -1,8 +1,19 @@
 import abc
+import enum
 
 from ....typehints import NUMERIC
 from ....types.transaction import Transaction
 from ...base import CpEndpoint
+
+
+class TrInitiatorCode(enum.IntEnum):
+    SERVICE_INITIATED = 0
+    CLIENT_INITIATED = 1
+
+
+class PaymentScheduled(enum.IntEnum):
+    ONCE = 0
+    SCHEDULED = 1
 
 
 class CpTokensChargeAuthEndpoint(CpEndpoint, abc.ABC):
@@ -17,3 +28,5 @@ class CpTokensChargeAuthEndpoint(CpEndpoint, abc.ABC):
     ip_address: str = None
     email: str = None
     json_data: dict = None
+    tr_initiator_code: int
+    payment_scheduled: int = None  # Note: required if tr_initiator_code is SERVICE_INITIATED
